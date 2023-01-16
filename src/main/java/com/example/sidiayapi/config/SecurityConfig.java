8052b03ec3b174bd8d66a6e7938173b7a47f4702
scx <scx0503@gmail.com>
2023-01-16 10:23:52 +0300

    Security init

--- src/main/java/com/example/sidiayapi/config/SecurityConfig.java
+++ src/main/java/com/example/sidiayapi/config/SecurityConfig.java
@@ -1,25 +1,29 @@
 package com.example.sidiayapi.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
     @Bean
     public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
-        UserDetails user = User.withUsername("user")
+        UserDetails user = User
+                .withUsername("user")
                 .password(passwordEncoder.encode("password"))
                 .roles("USER")
                 .build();
 
-        UserDetails admin = User.withUsername("admin")
+        UserDetails admin = User
+                .withUsername("admin")
                 .password(passwordEncoder.encode("admin"))
                 .roles("USER", "ADMIN")
                 .build();
@@ -30,11 +34,20 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .httpBasic()
+                .and()
                 .authorizeHttpRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .csrf()
+                .disable();
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
     }
 }
