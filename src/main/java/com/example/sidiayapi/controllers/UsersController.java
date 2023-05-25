3d5abea138315a1fafd79fd5f665d8aaaf26e895
scx <scx0503@gmail.com>
2023-05-25 08:35:51 +0300

    implemented getting ticket history

--- src/main/java/com/example/sidiayapi/controllers/UsersController.java
+++ src/main/java/com/example/sidiayapi/controllers/UsersController.java
@@ -6,7 +6,9 @@ import com.example.sidiayapi.services.UsersService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/users")
--- src/main/java/com/example/sidiayapi/enums/StatusesEnum.java
+++ src/main/java/com/example/sidiayapi/enums/StatusesEnum.java
@@ -1,7 +1,6 @@
 package com.example.sidiayapi.enums;
 
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public enum StatusesEnum {
     NOT_FORMED(1, "Не сформирована"),
